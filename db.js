bc537d2618825ed6e0b572bbedbe964c76c03522
Delphine Foo-Matkin <delphinef@gmail.com>
2016-01-04 12:49:26 -0800

    Set up schema and model for database, moved index.html and bower components to correct folder.

--- db.js
+++ db.js
@@ -4,3 +4,15 @@ var db = mongoose.connect('mongodb://localhost/fiveyear');
 mongoose.connection.once('connected', function() {
   console.log("Connected to database");
 });
+
+var entrySchema = new mongoose.Schema({
+  text: {
+    type: String,
+    maxlength: 140
+  },
+  createdAt: {
+    type: Date
+  }
+});
+
+var Entry = mongoose.model('Entry', entrySchema);
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var app = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var db = require('./db.js')
+var db = require('./db.js');
 
 var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/client'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
-
-app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));
-app.use(bodyParser.json());
